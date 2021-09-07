049b03a5f61d38de3199c3125882821eeccae042
Rajan Agarwal <rajan.ag005@gmail.com>
2021-09-07 11:10:58 -0400

    we value mental health

--- pages/components/Features.js
+++ pages/components/Features.js
@@ -69,15 +69,15 @@ export default function Features() {
               letterSpacing="tight"
               color={"gray.900"}
             >
-              <Text as={'span'} color={'green.400'} bgGradient="linear(to-l, #7928CA,#FF0080)" bgClip="text">
-              We Value Your Mental Health
+              <Text as={'span'} color={'green.400'} bgGradient="linear(to-l, #7928CA,#FF0080)" bgClip="text" fontSize={{ base: '30px', md: '40px' }}>
+              We Value Mental Health
             </Text>
             </chakra.p>
             <chakra.p
               mt={4}
               maxW="4xl"
               fontSize="xl"
-              mx={{ lg: "auto" }}
+              mx={{ base: 'auto', lg: "auto" }}
               color={"gray.400"}
             >
               *insert mission statement here*
@@ -94,56 +94,24 @@ export default function Features() {
             >
               <Feature
                 title="Action Title 1"
-                icon={
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                  />
-                }
               >
                 Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here <br/><br/>
               </Feature>
 
               <Feature
                 title="Action Title 2"
-                icon={
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
-                  />
-                }
               >
                 Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here <br/><br/>
               </Feature>
 
               <Feature
                 title="Action Title 2"
-                icon={
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
-                  />
-                }
               >
                 Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here <br/><br/>
               </Feature>
 
               <Feature
                 title="Action Title 2"
-                icon={
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
-                  />
-                }
               >
                 Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here Captivating Text Goes Here <br/><br/>
               </Feature>
